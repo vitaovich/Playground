ec84671d4d2d242e74c2b6511e90b58e2a02b7f0
Vitaliy Alekhnovich <vitaliyalekhnovich@vitoal-mbp.lan>
2020-07-23 17:03:20 -0700

    Add dependency injection of TodoContext in Startup Add TodoItem model Add script to scaffold TodoItemsController

--- C#/TodoApi.Scripts/Scripts ran.sh
+++ C#/TodoApi.Scripts/Scripts ran.sh
@@ -6,3 +6,9 @@ cd TodoApi
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 dotnet add package Microsoft.EntityFrameworkCore.InMemory
 code -r ../TodoApi
+
+#Scaffold a controller
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet tool install --global dotnet-aspnet-codegenerator
+dotnet aspnet-codegenerator controller -name TodoItemsController -async -api -m TodoItem -dc TodoContext -outDir Controllers

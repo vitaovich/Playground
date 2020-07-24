7f8693989c701d60b1aa5ec88cdca4ded8ee940d
Vitaliy Alekhnovich <vitaliyalekhnovich@vitoal-mbp.lan>
2020-07-23 17:17:15 -0700

    Test TodoItems Post method

--- C#/TodoApi.Scripts/Scripts ran.sh
+++ C#/TodoApi.Scripts/Scripts ran.sh
@@ -7,8 +7,25 @@ dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 dotnet add package Microsoft.EntityFrameworkCore.InMemory
 code -r ../TodoApi
 
+# can call web api through terminal using curl
+curl -k https://localhost:5001/weatherforecast
+
 #Scaffold a controller
 dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
 dotnet add package Microsoft.EntityFrameworkCore.Design
 dotnet tool install --global dotnet-aspnet-codegenerator
 dotnet aspnet-codegenerator controller -name TodoItemsController -async -api -m TodoItem -dc TodoContext -outDir Controllers
+
+# can post data using curl
+## post
+curl -k -H "Content-Type: application/json" 
+-d '
+{
+  "name":"walk dog",
+  "isComplete":true
+}' 
+https://localhost:5001/api/TodoItems
+
+## get
+curl -k https://localhost:5001/api/TodoItems # get list
+curl -k https://localhost:5001/api/TodoItems/1  # get TodoItems with id = 1
